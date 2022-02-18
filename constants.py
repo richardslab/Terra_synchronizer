0d053adfb8b592b3bab96b5bf071ec965e100bb7
Yossi Farjoun <farjoun@gmail.com>
2022-02-18 17:11:38 -0500

    localizer almost ready. just need to hook up the actual localizing part....

---
+++ constants.py
@@ -0,0 +1,38 @@
+# parts of a workspace
+import re
+
+NAMESPACE = "namespace"
+WORKSPACE = "workspace"
+ENTITY = "entity"
+
+# parts of a filter
+VALUES = "values"
+REGULAR_EXPRESSIONS = "regexps"
+HEAD = "head"
+
+# parts of a write
+OUTPUT = "output"
+DIRECTORY = "directory"
+
+# parts of a localize
+CRC32 = "crc32"
+LOCAL_PATH = "local_path"
+
+# parts of the data
+ATTRIBUTES = "attributes"
+
+# actions
+ACTION = "action"
+NAME = "name"
+SAMPLE = "sample"
+QUERY = "query"
+SUBSET = "subset"
+SELECT = "select"
+WRITE = "write"
+LOCALIZE = "localize"
+
+# REs
+# TODO: tighten these greatly (spaces etc...)
+GCS_GS_SCHEMA = re.compile("gs://.*")
+GCS_GS_BUCKET = re.compile("gs://([^/]*)/.*")
+GCS_GS_BLOB = re.compile("gs://[^/]*/(.*)")
--- main.py
+++ main.py
@@ -1,58 +1,179 @@
-# This is a sample Python script.
+import os
+from re import Pattern
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import firecloud.api as fapi
 import yaml
 import json
-import re
 from pprint import pprint
+from constants import *
+
+from google.cloud import storage
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def transpose_double_dict(double_dictionary: dict[str, dict]):
+    temp = {innerKey: {k: double_dictionary[k][innerKey] for k in double_dictionary if innerKey in double_dictionary[k]}
+            for
+            outerKey, outerDict in double_dictionary.items() for
+            innerKey, innerValue in outerDict.items()}
+    return temp
 
 
-def get_config(where: str):
-    with open(where, "tr") as file:
-        config = yaml.safe_load(file)
+def split_gs_uri(uri: str):
+    if not GCS_GS_SCHEMA.match(uri):
+        raise Exception(f"Not a gcs schema: {uri}")
+    bucket = GCS_GS_BUCKET.match(uri).group(1)
+    blob = GCS_GS_BLOB.match(uri).group(1)
+    return bucket, blob
 
-    pprint(config)
-    return (config)
 
+class NameFilter:
+    def __init__(self, values: list[str], regexps: list[str]):
+        self.values = values
+        self.regexps = regexps
 
-def entity_filter(config):
-    row_set: set = set(config["rows"]["values"])
-    re_set = set([re.compile(re_text) for re_text in config["rows"]["regexps"]])
+        self.values_set: set[str] = set(values)
+        self.re_set: set[Pattern] = set([re.compile(regexp) for regexp in regexps])
 
-    def filter_name_(name):
-        if name in row_set:
+    def filter(self, name):
+        if name in self.values_set:
             return True
-        for re_compiled in re_set:
+        for re_compiled in self.re_set:
             if re_compiled.match(name):
                 return True
         return False
 
-    return filter_name_
+
+def get_config(where: str):
+    with open(where, "tr") as file:
+        config_ = yaml.safe_load(file)
+    pprint(config_)
+    return config_
 
 
-def get_entities(config_file, namespace, workspace):
-    config = get_config("config.yml")
+def process_query(query, data):
+    pprint(query)
+    if len(data) != 0:
+        raise Exception(f"Input data has {len(data)} rows that will be over-written. Cowardly refusing to continue.")
+    r = fapi.get_entities(namespace=query[NAMESPACE], workspace=query[WORKSPACE],
+                          etype=query[ENTITY])
 
-    r = fapi.get_entities(namespace=namespace, workspace=workspace, etype=config["rows"]["entity"])
-    j = json.loads(r.content)
+    data = json.loads(r.content)
+    print(f"Found {len(data)} rows.")
+    return data
 
-    entityFilter = entity_filter(config)
 
-    output = [entity for entity in j if entityFilter(entity["name"])]
-    pprint(output)
-    return output
+def process_subset(subset, data: list):
+    pprint(subset)
+    filter_ = NameFilter(values=subset[VALUES], regexps=subset[REGULAR_EXPRESSIONS])
+    data = [datum for datum in data if filter_.filter(datum[NAME])]
+    print(f"Found {len(data)} rows.")
+    return data
+
+
+def process_sample(sample, data):
+    pprint(sample)
+    if HEAD in sample:
+        head = sample[HEAD]
+        print(f"Taking first {head} rows from data")
+        data = data[0:sample[HEAD]]
+        print(f"Found {len(data)} rows.")
+
+    else:
+        raise Exception(f"Action '{SAMPLE}' needs field '{HEAD}':" + str(sample))
+    return data
+
+
+def filter_attributes(datum: dict, filter_):
+    if ATTRIBUTES not in datum:
+        raise Exception(f"Cannot find key '{ATTRIBUTES}' in datum: {datum}")
+    datum[ATTRIBUTES] = {k: v for k, v in datum[ATTRIBUTES].items() if filter_(k)}
+    return datum
+
+
+def process_select(select, data):
+    pprint(select)
+
+    filter_ = NameFilter(values=select[VALUES], regexps=select[REGULAR_EXPRESSIONS])
+
+    data = [filter_attributes(datum, filter_.filter) for datum in data]
+    print(f"Found {len(data)} rows.")
+    if data:
+        print(f"Found {len(data[0][ATTRIBUTES])} attributes in the first row of the data.")
+    return data
+
+
+def process_write(write, data):
+    target = write[OUTPUT]
+    with open(target, 'wt') as file:
+        print(f"writing {len(data)} rows to {target}.")
+        file.write(json.dumps(data,indent=4))
+    return data
+
+
+def localize_blob(blob, path):
+    print(f"going to write blob to {path}")
+    return blob.crc32c
+
+
+def localize_possible_uri(client, datum, key, value, local_path):
+    if type(value) == str:
+        try:
+            bucket, blob = split_gs_uri(value)
+        except Exception:
+            return None
+        blob = client.get_bucket(bucket).get_blob(blob)
+
+        print(f"found blob {blob.name} in bucket {blob.bucket.name} with crc32 = '{blob.crc32c}'")
+        local_path = os.path.join(os.path.join(local_path, datum[NAME], key), os.path.basename(blob.name))
+        crc32 = localize_blob(blob, local_path)
+        assert crc32 == blob.crc32c
+        return {CRC32: crc32, LOCAL_PATH: local_path}
+
+
+def process_localize(localize, data: list[dict]):
+    local_path = localize[DIRECTORY]
+    client = storage.Client()
+
+    new_attributes = [
+        {key: localize_possible_uri(client, datum, key, value, local_path) for key, value in datum[ATTRIBUTES].items()}
+        for
+        datum in data]
+
+    # remove Nones
+    filtered_attributes = [{key: value for key, value in datum.items() if value is not None} for datum in
+                           new_attributes]
+
+    data_and_attributes = zip(data, filtered_attributes)
+    data = [datum | transpose_double_dict(new_attr) for datum, new_attr in data_and_attributes]
+
+    return data
+
+
+def process_config(config_):
+    data = []
+    process_switch = {
+        QUERY: process_query,
+        SUBSET: process_subset,
+        SAMPLE: process_sample,
+        SELECT: process_select,
+        WRITE: process_write,
+        LOCALIZE: process_localize
+    }
+
+    for action in config_:
+        if NAME in action:
+            print(action[NAME])
+        if ACTION not in action:
+            raise Exception("expecting an 'action' element in each entry of the config:" + str(action))
+        if action[ACTION] in process_switch:
+            data = process_switch[action[ACTION]](action, data)
+        else:
+            raise Exception("Don't know how to deal with action: %s" % action[ACTION])
+    pprint(data)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    entities = get_entities("config.yml", "brent-billing-project", "covid_RNA_qtl_v2")
-    pprint(entities)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    config = get_config("config.yml")
+
+    process_config(config)
